d1b192d5560f078d7e0900ca4b475e3b32b625f9
zk-fmf <zan.klanecek@fmf.uni-lj.si>
2020-12-07 16:23:35 +0100

    added comments and Model.eval() before validation

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ class Training():
         
         pos_weight = torch.Tensor([(negative/positive)]).cpu()
         
-        # 4. Creat train and validation generators, use mask, and batch_size = 10 for validation generator
+        # 4. Create train and validation generators, batch_size = 10 for validation generator (10 central slices)
         train_datareader = DataReader(self.main_path_to_data, train_info)
         train_generator = DataLoader(train_datareader, batch_size=16, shuffle=True, pin_memory=True, num_workers=2)
         
@@ -49,23 +49,24 @@ class Training():
         Model.cpu()
         
         # 6. Define criterion function, optimizer and scheduler
-        citerion_clf = torch.nn.BCEWithLogitsLoss(pos_weight)
+        citerion_clf = torch.nn.BCEWithLogitsLoss(pos_weight) # pos_weight for class imbalance
         optimizer = torch.optim.Adam(Model.parameters(), lr=learning_rate, weight_decay=weight_decay)
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, multiplicator, last_epoch=-1)
         
-        # 7. Run training
+        # 7. Creat lists for tracking AUC and Losses during training
         aucs = []
         losses = []
         best_auc = -np.inf
-                
         nb_batches = len(train_generator)
         
+        # 8. Run training
         for epoch in range(total_epoch):
             start = time.time()
             print('Epoch: %d/%d' % (epoch + 1, total_epoch))
             running_loss = 0
-            Model.train()  
             
+            # A) Train model
+            Model.train()  # put model in training mode
             for item_train in train_generator:  
                 # Forward pass          
                 optimizer.zero_grad()
@@ -73,35 +74,34 @@ class Training():
                 # Backward pass
                 loss.backward()
                 optimizer.step()
-                
                 # Track loss change
                 running_loss += loss.item()
             
-            # Calculate AUC on validation set
+            # B) Validate model
             predictions = []
             trues = []
+            
+            Model.eval() # put model in eval mode
             for item_valid in valid_generator:
                 prediction = Model.predict(item_valid, is_prob=True)
                 predictions.append(np.mean(prediction.cpu().numpy()))
                 trues.append(item_valid[1].numpy()[0])
         
             auc = roc_auc_score(trues, predictions)
-            aucs.append(auc)
-        
+            
+            # C) Track changes, update LR, save best model
             print("AUC: ", auc, ", Running loss: ", running_loss/nb_batches, ", Time: ", time.time()-start)
             
-            # If over 1/2 of epochs, and auc is the best auc so far, save this model as best model.
-            if (epoch >= total_epoch//2) and (auc > best_auc):
+            if (epoch >= total_epoch//2) and (auc > best_auc): # If over 1/2 of epochs and best AUC, save model as best model.
                 torch.save(Model.state_dict(), path_to_model + '/BEST_model.pth')
                 best_auc = auc         
             else:
                 pass
             
-            # Update losses
+            aucs.append(auc)
             losses.append(running_loss/nb_batches)
             scheduler.step()
             
-        
         np.save(path_to_model + '/AUCS.npy', np.array(aucs))
         np.save(path_to_model + '/LOSSES.npy', np.array(losses))
         torch.save(Model.state_dict(), path_to_model + '/LAST_model.pth')
